0fac6a79b51cbb699cc4b1ceae0083e54289acf6
GuilhermeDSGL <72310683+Guilherme-DSGL@users.noreply.github.com>
2023-01-10 10:39:41 -0300

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormBuilder,  FormControl,  Validators } from '@angular/forms';
+import { AuthService } from '../app-services/auth.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { User, userFromGroupForm } from './user';
+import { Router } from '@angular/router';
+import { openDialog } from '../pdv/template/dialog/dialog.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +14,49 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
       hidePassword = true;
+      user: User = new User();
+      @ViewChild('f') myNgForm: any;
+   
+      
+      constructor(
+        private fob: FormBuilder,
+        private service: AuthService,  
+        private router: Router,
+        private _snackBar: MatSnackBar
+      ){
+
+      }
+      formUser = this.fob.group(
+        {
+          name: ['', [Validators.required,Validators.minLength(3),]],
+          password: ['', [Validators.required]],
+        }
+      );
+
+   
+      getFieldIsValid(field: FormControl): boolean{
+        return field.invalid && (field.touched || field.dirty);
+      }
+    
+      resetForm(): void{
+        this.myNgForm.resetForm();
+      }
+
+      submit(){
+        this.user = userFromGroupForm(this.formUser);
+        this.authenticate();
+      }
+    
+      authenticate(){
+        this.service.authenticate(this.user).subscribe({
+         next:  response => {
+            localStorage.setItem('acess-token', JSON.stringify(response));
+            this.router.navigate(['/pdv']);
+         },
+         error: (error: HttpErrorResponse) => {
+            this._snackBar.open(error.message);
+         }
+        }
+        );
+      }
 }
