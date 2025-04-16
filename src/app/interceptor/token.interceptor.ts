a969eb9a831261718820be7e16c74f0c0bdda263
Guilherme de Souza <guilermeleitaoo@gmail.com>
2025-04-16 18:32:24 -0300

    feat: add title in tables

--- src/app/interceptor/token.interceptor.ts
+++ src/app/interceptor/token.interceptor.ts
@@ -12,25 +12,24 @@ export const BEARER = 'Bearer ';
 export const keyAcessToken = 'acess-token';
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-  
-  constructor() {}
+
+  constructor() { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     let jsonToken = JSON.parse(localStorage.getItem(keyAcessToken)!);
     console.log(jsonToken);
-    if(jsonToken){
-     let token = jsonToken.token;
-     if(request.url.search('auth') == -1){
-      console.log('fazendo fetch fora do auth')
-     request = request.clone(
-      {
-        setHeaders: {
-          AUTHORIZATION : BEARER + token,
-        }
+    if (jsonToken) {
+      let token = jsonToken.token;
+      if (request.url.search('auth') == -1) {
+        request = request.clone(
+          {
+            setHeaders: {
+              AUTHORIZATION: BEARER + token,
+            }
+          }
+        );
       }
-     );
     }
-  }
     return next.handle(request);
   }
 }
